1c3562d63f911edf386746f9c0ff8089f08b7bf1
annea <annearaujo034@gmail.com>
2022-09-20 01:23:13 -0300

    first commit

--- Exercicio7/src/funcionario/Main.java
+++ Exercicio7/src/funcionario/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		System.out.println("Salario do Gerente:" + gerente.Bonificacao());
 		System.out.println("Salario do Supervisor:" + supervisor.Bonificacao());
 		System.out.println("Salario do Vendedor:" + vendedor.Bonificacao());
+	
 	}
 
 }
